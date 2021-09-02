e24179de4801177b1d9e5f2bf51562e618529890
khalil saghour <saghourkhalil@gmail.com>
2021-09-02 15:24:13 +0200

    change date

--- src/app/Model/diplome.ts
+++ src/app/Model/diplome.ts
@@ -4,9 +4,9 @@ export  class Diplome{
   public school:string;
   public place:string;
   public resume:string;
-  public gratuatedDate: string;
+  public gratuatedDate: Date;
 
-  constructor(id: number, diplome: string, school: string, place: string, resume: string, gratuatedDate: string) {
+  constructor(id: number, diplome: string, school: string, place: string, resume: string, gratuatedDate: Date) {
     this.id = id;
     this.diplome = diplome;
     this.school = school;
@@ -14,6 +14,7 @@ export  class Diplome{
     this.resume = resume;
     this.gratuatedDate = gratuatedDate;
   }
+
 
 
 
